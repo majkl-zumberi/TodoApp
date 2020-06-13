1fa110bb886682cd46d8f694ea15b0206e9a8467
majkl zumberi <majklzumberi00@gmail.com>
2020-06-13 19:20:34 +0200

    todo detail logic

--- src/app/features/todos/components/todo-detail/todo-detail.component.ts
+++ src/app/features/todos/components/todo-detail/todo-detail.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Todo } from 'src/app/core/model/todo.interface';
+import { TodosFacadeService } from '../services/todos-facade.service';
+
 @Component({
   selector: 'app-todo-detail',
   templateUrl: './todo-detail.component.html',
@@ -7,9 +12,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoDetailComponent implements OnInit {
 
-  constructor() { }
+  get todo(): Observable<Todo> {
+    return this.todosFacadeService.todoSelected$;
+  }
+
+  constructor(private todosFacadeService: TodosFacadeService, private route: ActivatedRoute) {
+  }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      if(params != null && params['id'] != null){
+        this.todosFacadeService.getTodoById(params['id']);
+      }
+    });
+  }
+
+  edit(todo: Todo){
+    this.todosFacadeService.goToEdit(todo.id);
   }
 
 }
