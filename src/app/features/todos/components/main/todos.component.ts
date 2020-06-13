35b2b94f19ad054f7c0fdafbbefef3aba6df921b
majkl zumberi <majklzumberi00@gmail.com>
2020-06-13 19:21:08 +0200

    added logic to todos comp

--- src/app/features/todos/components/main/todos.component.ts
+++ src/app/features/todos/components/main/todos.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable } from 'rxjs';
+import { Todo } from 'src/app/core/model/todo.interface';
+import { TodosFacadeService } from '../services/todos-facade.service';
+
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
@@ -7,9 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosComponent implements OnInit {
 
-  constructor() { }
+  get todosList(): Observable<Todo[]> {
+    return this.todosFacadeService.tods$;
+  }
+
+  constructor(private todosFacadeService: TodosFacadeService) { }
 
   ngOnInit(): void {
+    this.todosFacadeService.getAllTodos();
+  }
+
+  showDetail(todo: Todo) {
+    this.todosFacadeService.goToDetail(todo.id);
   }
 
 }
