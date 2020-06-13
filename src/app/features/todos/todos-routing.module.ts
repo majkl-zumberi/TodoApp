2adfd8c0c5c09ef99e30fce7673fe37001fb2b4e
majkl zumberi <majklzumberi00@gmail.com>
2020-06-13 19:22:37 +0200

    todo routing & module (lazy)

--- src/app/features/todos/todos-routing.module.ts
+++ src/app/features/todos/todos-routing.module.ts
@@ -2,8 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { TodosComponent } from './components/main/todos.component';
+import { TodoEditComponent } from './components/todo-edit/todo-edit.component';
+import { TodoDetailComponent } from './components/todo-detail/todo-detail.component';
 
-const routes: Routes = [{ path: '', component: TodosComponent }];
+const routes: Routes = [{   path: '', component: TodosComponent, children: [
+  {path: 'detail/:id', component: TodoDetailComponent},
+  {path: 'edit/:id', component: TodoEditComponent}
+]
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/features/todos/todos.module.ts
+++ src/app/features/todos/todos.module.ts
@@ -5,13 +5,17 @@ import { TodosRoutingModule } from './todos-routing.module';
 import { TodosComponent } from './components/main/todos.component';
 import { TodoEditComponent } from './components/todo-edit/todo-edit.component';
 import { TodoDetailComponent } from './components/todo-detail/todo-detail.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { TodosFacadeService } from './components/services/todos-facade.service';
 
 
 @NgModule({
   declarations: [TodosComponent, TodoEditComponent, TodoDetailComponent],
+  providers: [TodosFacadeService],
   imports: [
     CommonModule,
-    TodosRoutingModule
+    TodosRoutingModule,
+    SharedModule
   ]
 })
 export class TodosModule { }
