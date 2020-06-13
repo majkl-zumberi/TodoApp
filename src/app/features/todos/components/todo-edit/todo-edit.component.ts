2082d518c5ea730d0f775e6f850188287f5e6af4
majkl zumberi <majklzumberi00@gmail.com>
2020-06-13 19:20:20 +0200

    added todo edit logic

--- src/app/features/todos/components/todo-edit/todo-edit.component.ts
+++ src/app/features/todos/components/todo-edit/todo-edit.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Todo } from 'src/app/core/model/todo.interface';
+import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { TodosFacadeService } from '../services/todos-facade.service';
 
 @Component({
   selector: 'app-todo-edit',
@@ -7,9 +13,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoEditComponent implements OnInit {
 
-  constructor() { }
+  get todo(): Observable<Todo> {
+    return this.todosFacadeService.todoSelected$;
+  }
+
+  constructor(private todosFacadeService: TodosFacadeService, private route: ActivatedRoute) {
+  }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      if(params != null && params['id'] != null){
+        this.todosFacadeService.getTodoById(params['id']);
+      }
+    });
+  }
+
+  editForm(todo: Todo) {
+    this.todosFacadeService.editTodo(todo);
+  }
+
+  undo(todo: Todo) {
+    this.todosFacadeService.goToDetail(todo.id);
   }
 
 }
