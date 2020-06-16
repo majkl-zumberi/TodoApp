bdf5e4990aed846dc6f299e46bd10a9c6c4b1452
majkl zumberi <majklzumberi00@gmail.com>
2020-06-16 11:32:05 +0200

    commentato per il momento l'interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+//import { HttpIntercept } from './core/services/http-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,10 @@ import { CoreModule } from './core/core.module';
     AppRoutingModule,
     CoreModule
   ],
-  providers: [],
+  providers: [
+    //{ provide: ErrorHandler, useClass: SentryErrorHandler },
+    //{ provide: HTTP_INTERCEPTORS, useClass: HttpIntercept, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
