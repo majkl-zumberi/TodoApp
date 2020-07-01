050c2ffb8607a48a711257e9db27162bccbfc662
majkl zumberi <majklzumberi00@gmail.com>
2020-07-01 13:01:51 +0200

    installed dev dep. store dev tools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,10 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
-import { reducer } from './redux/totos.reducers';
+import { reducer } from './redux/todos.reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment'; // Angular CLI environment
+import { reducers } from './redux/index';
 //import { HttpIntercept } from './core/services/http-interceptor.service';
 
 @NgModule({
@@ -17,7 +20,11 @@ import { reducer } from './redux/totos.reducers';
     BrowserModule,
     AppRoutingModule,
     CoreModule,
-    StoreModule.forRoot({ todoState: reducer })
+    StoreModule.forRoot(reducers),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    })
   ],
   providers: [
     //{ provide: ErrorHandler, useClass: SentryErrorHandler },
