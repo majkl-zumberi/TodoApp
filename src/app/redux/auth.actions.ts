c34579ca7fd8a17ef3d9c32bd0d6ed8d37e33b77
majkl zumberi <majklzumberi00@gmail.com>
2020-07-01 12:54:18 +0200

    created auth actions for ngrx

---
+++ src/app/redux/auth.actions.ts
@@ -0,0 +1,6 @@
+import { createAction, props} from '@ngrx/store';
+import { User } from '../core/model/user.interface';
+export const initUser  =createAction('[User] init',  props<{user:User}>());
+export const insertUser =createAction('[User] insert',props<{user:User}>());
+export const removeUser =createAction('[User] remove',props<{id:number}>());
+export const editUser   =createAction('[User] edit',  props<{user:User}>());
