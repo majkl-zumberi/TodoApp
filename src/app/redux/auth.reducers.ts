08d20ceae43840bfb1e5eec4f1500d1593f71622
majkl zumberi <majklzumberi00@gmail.com>
2020-07-01 12:54:01 +0200

    created auth reducer

---
+++ src/app/redux/auth.reducers.ts
@@ -0,0 +1,25 @@
+import { Action, createReducer, on } from '@ngrx/store';
+import { User } from '../core/model/user.interface';
+import { initUser, insertUser, removeUser, editUser } from './auth.actions';
+
+
+export interface UserState {
+  user: User;
+}
+
+export const initialState: UserState = {
+    user:null
+};
+
+
+export const authReducer = createReducer(
+    initialState,
+    on(initUser, (state,{user}) => ({ ...state, initUser:user })),
+    //on(insertTodo,(state,{todo}) => ({ ...state,todos:[...state.todos,todo] })),
+    //on(removeTodo,(state,{id}) => ({ ...state, todos:state.todos.filter(item=>item.id !==id) })),
+    //on(editTodo,(state,{todo}) => ({ ...state,todos:state.todos.map(item=>item.id===todo.id?todo:item) }))
+  );
+
+  export function reducer(state: UserState | undefined, action: Action) {
+    return authReducer(state, action);
+  }
