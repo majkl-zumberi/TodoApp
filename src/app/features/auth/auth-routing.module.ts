80fbca384e78e73fac4fe40775dbda41f2551a26
majkl zumberi <majklzumberi00@gmail.com>
2020-07-01 10:43:44 +0200

    added auth lazy module

---
+++ src/app/features/auth/auth-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AuthComponent } from './components/main/auth.component';
+import { LoginComponent } from './components/login/login.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
+  { path: '', component: AuthComponent, children : [
+  { path: 'login', component: LoginComponent},
+  //{ path: 'register', component: RegisterComponent}
+] }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule { }
