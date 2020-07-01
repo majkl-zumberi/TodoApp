53028ef855c7b9802d9358aa593106241c5f1cd4
majkl zumberi <majklzumberi00@gmail.com>
2020-07-01 10:44:37 +0200

    updated some routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CanLoadCompsGuard } from './core/guards/can-load-comps-guard.guard';
+import { CanloadloginGuard } from './core/guards/canloadlogin.guard';
 
 
 const routes: Routes = [
-  { path: 'todos', loadChildren: () => import('./features/todos/todos.module').then(m => m.TodosModule) },
-  { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule)},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'todos', loadChildren: () => import('./features/todos/todos.module').then(m => m.TodosModule), canLoad:[CanLoadCompsGuard] },
+  { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule), canLoad:[CanLoadCompsGuard]},
+  { path: 'auth', loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule), canLoad:[CanloadloginGuard] },
 ];
 
 @NgModule({
