bf4ca97fdde3f501269367cc96d96feb09bc4220
majkl zumberi <majklzumberi00@gmail.com>
2020-07-05 16:53:11 +0200

    added 404 & shared lazy module to the routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CanLoadCompsGuard } from './core/guards/can-load-comps-guard.guard';
 import { CanloadloginGuard } from './core/guards/canloadlogin.guard';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
@@ -9,6 +10,8 @@ const routes: Routes = [
   { path: 'todos', loadChildren: () => import('./features/todos/todos.module').then(m => m.TodosModule), canLoad:[CanLoadCompsGuard] },
   { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule), canLoad:[CanLoadCompsGuard]},
   { path: 'auth', loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule), canLoad:[CanloadloginGuard] },
+  { path: 'shared-with-me', loadChildren: () => import('./features/tods-shared/tods-shared.module').then(m => m.TodsSharedModule), canLoad:[CanLoadCompsGuard] },
+  {path:'**',component: PageNotFoundComponent},
 ];
 
 @NgModule({
