4e4e24472fa7ec033dbedbccdb262fb4771a7142
majkl zumberi <majklzumberi00@gmail.com>
2020-07-05 16:41:36 +0200

    added toggle btn & handle logic & event emitter for that event

--- src/app/shared/components/todo-preview/todo-preview.component.ts
+++ src/app/shared/components/todo-preview/todo-preview.component.ts
@@ -1,12 +1,13 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { Todo } from 'src/app/core/model/todo.interface';
+import { User } from 'src/app/core/model/user.interface';
 
 @Component({
   selector: 'app-todo-preview',
   templateUrl: './todo-preview.component.html',
   styleUrls: ['./todo-preview.component.scss']
 })
-export class TodoPreviewComponent {
+export class TodoPreviewComponent implements OnInit{
 
   @Input()
   todo: Todo;
@@ -15,9 +16,16 @@ export class TodoPreviewComponent {
   detailEvent: EventEmitter<void> = new EventEmitter();
   @Output()
   deleteEvent: EventEmitter<void> = new EventEmitter();
+  @Output()
+  toggleEvent: EventEmitter<Boolean> = new EventEmitter<Boolean>();
+
+  toggle:boolean=false;
 
   constructor() { }
 
+  ngOnInit(): void {
+    this.toggle=this.todo.forUser.some(t=>t.username==(JSON.parse(sessionStorage.getItem('utente')) as User)?.username)?true:false??false;
+  }
   detailClick() {
     this.detailEvent.emit();
   }
@@ -25,5 +33,10 @@ export class TodoPreviewComponent {
     console.log(`user try to delete ${todo.title}`);
     this.deleteEvent.emit();
   }
-
+  toggleBtn(){
+    console.log("toggle");
+    this.toggle=!this.toggle;
+    console.log(this.toggle);
+    this.toggleEvent.emit(this.toggle);
+  }
 }
