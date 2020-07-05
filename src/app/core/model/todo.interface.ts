2f073f5c1809cc7bf422c1f2be891e5fbf6a42cd
majkl zumberi <majklzumberi00@gmail.com>
2020-07-05 16:32:56 +0200

    edit: added for User attribute for shared todos

--- src/app/core/model/todo.interface.ts
+++ src/app/core/model/todo.interface.ts
@@ -1,8 +1,10 @@
 import { TodoStep } from './todo-step.interface';
+import { forUser } from './todo-forUser.interface';
 
 export interface Todo {
     id: number;
     title: string;
     description: string;
     steps: TodoStep[];
+    forUser:forUser[];
 }
