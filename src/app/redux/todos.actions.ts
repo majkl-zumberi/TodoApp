49064be3139cc218d0c559b6f5bd9a35bd9b2a58
majkl zumberi <majklzumberi00@gmail.com>
2020-07-08 16:16:31 +0200

    moved into todo folder & added effects actions

--- src/app/redux/todos.actions.ts
+++
@@ -1,6 +0,0 @@
-import { createAction, props} from '@ngrx/store';
-import { Todo } from '../core/model/todo.interface';
-export const initTodos  =createAction('[Todos] init',  props<{todos:Todo[]}>());
-export const insertTodo =createAction('[Todos] insert',props<{todo:Todo}>());
-export const removeTodo =createAction('[Todos] remove',props<{id:number}>());
-export const editTodo   =createAction('[Todos] edit',  props<{todo:Todo}>());
---
+++ src/app/redux/todos/todos.actions.ts
@@ -0,0 +1,12 @@
+import { createAction, props} from '@ngrx/store';
+import { Todo } from '../../core/model/todo.interface';
+//store
+export const initTodos          =createAction('[Todos] store init',  props<{todos:Todo[]}>());
+export const insertTodo         =createAction('[Todos] store insert',props<{todo:Todo}>());
+export const removeTodo         =createAction('[Todos] store remove',props<{id:number}>());
+export const editTodo           =createAction('[Todos] store edit',  props<{todo:Todo}>());
+//effects
+export const retreiveAllTodos     =createAction('[Todos] effect - get all');
+export const updateTodo           =createAction('[Todos] effect - update',props<{todo:Todo}>());
+export const insertTodoEff        =createAction('[Todos] effect - insert',props<{todo:Todo}>());
+export const DeleteTodo           =createAction('[Todos] effect - remove',props<{id:number}>());
