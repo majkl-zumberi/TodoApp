1e2ac94e4b14907ee31ba2a44852d1932efc2090
majkl zumberi <majklzumberi00@gmail.com>
2020-07-09 18:28:46 +0200

    added register comp under auth lazy module | custom validator

---
+++ src/app/features/auth/components/register/register.component.ts
@@ -0,0 +1,70 @@
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators,
+  AbstractControl
+} from '@angular/forms';
+import {
+  Subscription
+} from 'rxjs';
+import {
+  AuthFacadeService
+} from '../services/auth-facade.service';
+import { debounceTime } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+
+  passwordMessage: string;
+  registerForm: FormGroup;
+  get usernameControl(): FormControl {
+    return this.registerForm.get('username') as FormControl;
+  }
+
+  constructor(private fb: FormBuilder, private authFacadeService: AuthFacadeService) {
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      pswGroup: this.fb.group({
+        password: ['', Validators.required],
+        passwordCnf: ['', Validators.required],
+      }, {
+        validator: this.checkPasswords
+      })
+    });
+  }
+  registerUser() {
+    console.log("username:" + this.registerForm.get("username").value);
+    console.log("password:" + this.registerForm.get("pswGroup").get("password").value);
+    this.authFacadeService.signUp(this.registerForm.get("username").value,this.registerForm.get("pswGroup").get("password").value);
+  }
+
+  checkPasswords(c: AbstractControl): { [key: string]: boolean } | null {
+    const emailControl = c.get('password');
+    const confirmControl = c.get('passwordCnf');
+
+    if (emailControl.pristine || confirmControl.pristine) {
+      return null;
+    }
+
+    if (emailControl.value === confirmControl.value) {
+      return null;
+    }
+    return { match: true };
+  }
+
+
+
+  ngOnInit(): void {
+
+  }
+
+}
