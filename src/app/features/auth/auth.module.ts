13b49ab9faaf9cea21ac9da1fc5e156c9338124b
majkl zumberi <majklzumberi00@gmail.com>
2020-07-09 18:27:54 +0200

    added register component

--- src/app/features/auth/auth.module.ts
+++ src/app/features/auth/auth.module.ts
@@ -5,10 +5,11 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './components/main/auth.component';
 import { LoginComponent } from './components/login/login.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { RegisterComponent } from './components/register/register.component';
 
 
 @NgModule({
-  declarations: [AuthComponent,LoginComponent],
+  declarations: [AuthComponent,LoginComponent, RegisterComponent],
   imports: [
     CommonModule,
     AuthRoutingModule,
