04538cf3ede5a0fc4b4cb88ecc0bb8bf7cf8c64d
majkl zumberi <majklzumberi00@gmail.com>
2020-07-13 18:30:36 +0200

    registered authEffects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,24 +10,28 @@ import { reducer } from './redux/todos/todos.reducers';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment'; // Angular CLI environment
 import { reducers } from './redux/index';
-import { MenuComponent } from './components/menu/menu.component';
+
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { EffectsModule } from '@ngrx/effects';
 import { TodosEffects } from './redux/todos/todos.effects';
+import { MenuComponent } from './components/menu/menu.component';
+import { ProfileRenderComponent } from './shared/components/profile-render/profile-render.component';
+import { authEffects } from './redux/user/auth.effects';
 //import { HttpIntercept } from './core/services/http-interceptor.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    ProfileRenderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CoreModule,
     StoreModule.forRoot(reducers),
-    EffectsModule.forRoot([TodosEffects]),
+    EffectsModule.forRoot([TodosEffects,authEffects]),
     StoreDevtoolsModule.instrument({
       maxAge: 25, // Retains last 25 states
       logOnly: environment.production, // Restrict extension to log-only mode
