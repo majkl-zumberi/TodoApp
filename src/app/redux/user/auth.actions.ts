cf85cb99ddac7f3ed6c381776f1c24ebbfd5c896
majkl zumberi <majklzumberi00@gmail.com>
2020-07-14 19:23:52 +0200

    added loginuser,success,failure actions

--- src/app/redux/user/auth.actions.ts
+++ src/app/redux/user/auth.actions.ts
@@ -5,5 +5,11 @@ export const insertUser =createAction('[User] insert',props<{user:User}>());
 export const removeUser =createAction('[User] remove',props<{}>());
 export const editUser   =createAction('[User] edit',  props<{user:User}>());
 
-//effect
+//effects
+//login
+export const loginUser=createAction('[Auth] Login',  props<{username:string, password:string}>());
+export const loginUserSuccess=createAction('[Auth] Login Success',props<{user:User}>());
+export const loginUserFailure=createAction('[Auth] Login Failure',props<{error:string}>());
+
+//update user profile
 export const updateUser=createAction('[User] Effect: edit',  props<{user:User}>());
