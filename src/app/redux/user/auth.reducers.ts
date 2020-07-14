589527a2613133e656d34a4da3d74cb46d65e9b9
majkl zumberi <majklzumberi00@gmail.com>
2020-07-14 19:24:38 +0200

    added error message in UserState & handled loginFailure

--- src/app/redux/user/auth.reducers.ts
+++ src/app/redux/user/auth.reducers.ts
@@ -1,21 +1,25 @@
-import { Action, createReducer, on } from '@ngrx/store';
+import { Action, createReducer, on, State } from '@ngrx/store';
 import { User } from '../../core/model/user.interface';
-import { initUser, insertUser, removeUser, editUser } from './auth.actions';
+import { initUser, insertUser, removeUser, editUser,loginUserFailure } from './auth.actions';
 
 
 export interface UserState {
   user: User;
+  // error message
+  errorMessage: string | null;
 }
 
 export const initialState: UserState = {
-    user:JSON.parse(sessionStorage.getItem("utente"))as User
+    user:JSON.parse(sessionStorage.getItem("utente"))as User,
+    errorMessage: null
 };
 
 
 export const authReducer = createReducer(
     initialState,
-    on(initUser, (state,{user}) => ({ ...state, user:user })),
+    on(initUser, (state,{user}) => ({ ...state, user:user,errorMessage:null })),
     //on(insertTodo,(state,{todo}) => ({ ...state,todos:[...state.todos,todo] })),
+    on(loginUserFailure,(state,{error})=>({...state,user:null,errorMessage:error})),
     on(removeUser,(state) => ({ ...state, user:null })),
     on(editUser,(state,{user}) => ({ ...state,user:user }))
   );
