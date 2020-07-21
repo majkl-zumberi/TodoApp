4168930fdbb7c5dd0e1b322ee62b366da584e00c
majkl zumberi <majklzumberi00@gmail.com>
2020-07-21 17:25:42 +0200

    added http client module

--- src/app/features/auth/auth.module.ts
+++ src/app/features/auth/auth.module.ts
@@ -6,14 +6,15 @@ import { AuthComponent } from './components/main/auth.component';
 import { LoginComponent } from './components/login/login.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { RegisterComponent } from './components/register/register.component';
-
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AuthComponent,LoginComponent, RegisterComponent],
   imports: [
     CommonModule,
     AuthRoutingModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule
   ]
 })
 export class AuthModule { }
