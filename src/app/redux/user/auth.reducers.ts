da5db268acbc00d49867b3cf479bf42f722f4d6c
majkl zumberi <majklzumberi00@gmail.com>
2020-07-25 19:25:11 +0200

    some logic for selecting all UsersUsernames | MultiSelect | syncfusion

--- src/app/redux/user/auth.reducers.ts
+++ src/app/redux/user/auth.reducers.ts
@@ -1,17 +1,19 @@
 import { Action, createReducer, on, State } from '@ngrx/store';
 import { User } from '../../core/model/user.interface';
-import { initUser, insertUser, removeUser, editUser,loginUserFailure } from './auth.actions';
+import { initUser, insertUser, removeUser, editUser,loginUserFailure, usersUsername } from './auth.actions';
 
 
 export interface UserState {
   user: User;
+  usersUsername:string[];
   // error message
   errorMessage: string | null;
 }
 
 export const initialState: UserState = {
     user:JSON.parse(sessionStorage.getItem("utente"))as User,
-    errorMessage: null
+    errorMessage: null,
+    usersUsername:['']
 };
 
 
@@ -21,7 +23,8 @@ export const authReducer = createReducer(
     //on(insertTodo,(state,{todo}) => ({ ...state,todos:[...state.todos,todo] })),
     on(loginUserFailure,(state,{error})=>({...state,user:null,errorMessage:error})),
     on(removeUser,(state) => ({ ...state, user:null })),
-    on(editUser,(state,{user}) => ({ ...state,user:user }))
+    on(editUser,(state,{user}) => ({ ...state,user:user })),
+    on(usersUsername,(state,{usernames})=>({...state,usersUsername:usernames}))
   );
 
   export function reducer(state: UserState | undefined, action: Action) {
