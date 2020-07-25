adb9b663df6459409da7f9b3f12bc0840bd5494e
majkl zumberi <majklzumberi00@gmail.com>
2020-07-25 19:27:38 +0200

    deleted onInit,onDestroy | converted into async | usage of @ngrx router store

--- src/app/features/todos/components/todo-detail/todo-detail.component.ts
+++ src/app/features/todos/components/todo-detail/todo-detail.component.ts
@@ -6,32 +6,22 @@ import { Todo } from 'src/app/core/model/todo.interface';
 import { TodosFacadeService } from '../services/todos-facade.service';
 import { filter, switchMap } from 'rxjs/operators';
 import { select, Store } from '@ngrx/store';
-import { getTodoById } from 'src/app/redux';
+import { getTodoById, getCurrentNavigatedTodo } from 'src/app/redux';
 
 @Component({
   selector: 'app-todo-detail',
   templateUrl: './todo-detail.component.html',
   styleUrls: ['./todo-detail.component.scss']
 })
-export class TodoDetailComponent implements OnInit,OnDestroy {
-  private subscription: Subscription = new Subscription();
-  todo:Todo;
+export class TodoDetailComponent {
+  get todo():Observable<Todo>{
+    return this.store.pipe(select(getCurrentNavigatedTodo));
+  };
 
 
   constructor(private todosFacadeService: TodosFacadeService, private route: ActivatedRoute,private store:Store) {
   }
 
-  ngOnInit(): void {
-    this.subscription.add(this.route.params.pipe(
-      filter(params => params != null && params['id'] != null),
-      switchMap(params => this.store.pipe(select(getTodoById, { id: Number(params['id']) }))),
-    ).subscribe(todo => {
-      this.todo = todo;
-    }));
-  }
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
   edit(todo: Todo){
     this.todosFacadeService.goToEdit(todo.id);
   }
