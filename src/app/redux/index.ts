b2b0dbce605f3387bb092f00302e0e88f0b609a2
majkl zumberi <majklzumberi00@gmail.com>
2020-07-25 19:18:30 +0200

    added router state logic (current navigated todo) & select all usersUsernames

--- src/app/redux/index.ts
+++ src/app/redux/index.ts
@@ -1,20 +1,37 @@
-import { createSelector, ActionReducerMap } from '@ngrx/store';
+import { createSelector, ActionReducerMap, createFeatureSelector } from '@ngrx/store';
 import { TodoState,todosReducer } from './todos/todos.reducers';
 import { UserState, authReducer } from './user/auth.reducers';
 import { User } from '../core/model/user.interface';
+import { RouterReducerState, routerReducer, getSelectors } from '@ngrx/router-store';
+import { Params } from '@angular/router';
 
 
 export interface AppState {
   todoState:TodoState;
   authState:UserState;
+  router:RouterReducerState<any>
 }
 export const reducers: ActionReducerMap<AppState> = {
   todoState: todosReducer,
-  authState: authReducer
+  authState: authReducer,
+  router:routerReducer
 };
 export const selectTodoState = (state: AppState) => state.todoState;
 export const selectUserState = (state: AppState) => state.authState;
+export const selectRouter = createFeatureSelector<
+  AppState,
+  RouterReducerState<any>
+>('router');
 
+export const {
+  selectCurrentRoute,   // select the current route
+  selectQueryParams,    // select the current route query params
+  selectQueryParam,     // factory function to select a query param
+  selectRouteParams,    // select the current route params
+  selectRouteParam,     // factory function to select a route param
+  selectRouteData,      // select the current route data
+  selectUrl,            // select the current url
+} = getSelectors(selectRouter);
 export const selectTodos = createSelector(
  selectTodoState,
   (state: TodoState) => state.todos
@@ -36,6 +53,10 @@ export const usersListOfTodo=createSelector(
       })
   })
 
+export const selectAllUsersUsername=createSelector(
+  selectUserState,
+  (UserState:UserState)=>UserState.usersUsername.map(username=>({Name:username,Code:username}))
+);
 
 export const selectTodosAssigned = createSelector(
   selectTodoState,
@@ -50,7 +71,11 @@ export const getTodoById = createSelector(
     selectTodoState,
     (state: TodoState, props: { id: number }) => state.todos.find(item => item.id === props.id)
 );
-
+export const getCurrentNavigatedTodo=createSelector(
+    selectTodoState,
+    selectRouteParams,
+    (state:TodoState, params:Params)=>state.todos.find(item=> item.id=== Number(params['id']))
+);
 export const getFirstTodo = createSelector(
     selectTodoState,
     (state: TodoState) => state.todos.length > 0 ? state.todos[0] : null
