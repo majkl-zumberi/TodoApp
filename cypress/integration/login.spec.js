4aac6f33b21c2496534e9e6b92979e140384007c
majkl zumberi <majklzumberi00@gmail.com>
2020-07-27 19:43:28 +0200

    added checks form content in login.spec

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -3,34 +3,51 @@ context('login', () => {
     sessionStorage.removeItem('utente');
     cy.visit('http://localhost:4200/');
   })
+  it('checks form content', () => {
+    cy.get('form').within($form => {
+      cy.wrap($form).should('be.visible')
+      cy.wrap($form).children().should('have.length', 4)
+      cy.wrap($form).find('button').last().should('be.disabled').and('have.text','Login').and('have.class','uk-button');
+      cy.get('.username-input').type('majjey').should('have.value', 'majjey').and('have.focus');
+      cy.get('.password-input').type('123').should('have.value', '123').and('have.focus');
+      cy.wrap($form).find('.uk-text-small').last().should('have.text','Non sei registrato? Crea un account')
+      cy.wrap($form).find('.uk-text-small').find('a').last().should('have.attr','routerlink','/auth/register')
+    })
+  });
+  it('submit login', () => {
+    cy.url().should('eq', 'http://localhost:4200/auth/login')
+    cy.contains('Ben tornato!')
+    cy.get('form').within($form => {
+      cy.get('.username-input').type('majjey').should('have.value', 'majjey').and('have.focus');
+      cy.get('.password-input').type('123').should('have.value', '123').and('have.focus');
 
-  it('submit login',()=>{
-    cy.url().should('eq','http://localhost:4200/auth/login')
-    cy.get('.username-input').type('majjey').should('have.value','majjey');
-    cy.get('.password-input').type('123').should('have.value','123');
-    cy.server();
-    const baseURL='http://localhost:3000/';
-    cy.route('GET',baseURL+'users').as('LoginCall');
-    cy.get('.uk-button').click();
-    //cy.get('.welcome-title').should('have.text','MyTodosApp');
-    cy.wait('@LoginCall').then(()=>{
-      cy.get('.welcome-title').should('have.text','MyTodosApp');
+      cy.server();
+      const baseURL = 'http://localhost:3000/';
+
+      cy.route('GET', baseURL + 'users').as('LoginCall');
+      // cy.get('.uk-button').click();
+      cy.root().submit()
+      //cy.get('.welcome-title').should('have.text','MyTodosApp');
+
+    })
+    cy.wait('@LoginCall').then(() => {
+      //cy.get('.welcome-title').should('have.text','MyTodosApp');
       cy.contains('MyTodosApp')
-      cy.url().should('eq','http://localhost:4200/home')
+      cy.url().should('eq', 'http://localhost:4200/home')
       // cy.url().should('include','home')
     });
   });
   it('submit error login', () => {
-    cy.url().should('eq','http://localhost:4200/auth/login')
-    cy.get('.username-input').type('non esiste').should('have.value','non esiste');
-    cy.get('.password-input').type('1234').should('have.value','1234');
+    cy.url().should('eq', 'http://localhost:4200/auth/login')
+    cy.get('.username-input').type('non esiste').should('have.value', 'non esiste');
+    cy.get('.password-input').type('1234').should('have.value', '1234');
     cy.server();
-    const baseURL='http://localhost:3000/';
-    cy.route('GET',baseURL+'users').as('LoginCall');
+    const baseURL = 'http://localhost:3000/';
+    cy.route('GET', baseURL + 'users').as('LoginCall');
     cy.get('.uk-button').click();
     //cy.get('.welcome-title').should('have.text','MyTodosApp');
-    cy.wait('@LoginCall').then(()=>{
-      cy.get('#loginError').should('have.text','username e/o password non corretta');
+    cy.wait('@LoginCall').then(() => {
+      cy.get('#loginError').should('have.text', 'username e/o password non corretta');
     });
   });
 
