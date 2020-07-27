4b4a70de1c9b0544a8158fec301f852d57c6c01f
majkl zumberi <majklzumberi00@gmail.com>
2020-07-27 21:44:31 +0200

    added custom login command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,28 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+Cypress.Commands.add('Login',()=>{
+  sessionStorage.removeItem('utente');
+  cy.visit("");
+  cy.url().should('eq', 'http://localhost:4200/auth/login')
+    cy.contains('Ben tornato!')
+    cy.get('form').within($form => {
+      cy.get('.username-input').type('majjey').should('have.value', 'majjey').and('have.focus');
+      cy.get('.password-input').type('123').should('have.value', '123').and('have.focus');
+
+      cy.server();
+      const baseURL = 'http://localhost:3000/';
+
+      cy.route('GET', baseURL + 'users').as('LoginCall');
+      // cy.get('.uk-button').click();
+      cy.root().submit()
+      //cy.get('.welcome-title').should('have.text','MyTodosApp');
+
+    })
+    cy.wait('@LoginCall').then(() => {
+      //cy.get('.welcome-title').should('have.text','MyTodosApp');
+      cy.contains('MyTodosApp')
+      cy.url().should('eq', 'http://localhost:4200/home')
+      // cy.url().should('include','home')
+    });
+})
