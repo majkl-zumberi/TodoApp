80cbd7154277885e8e815e74cc516437c19cd49a
majkl zumberi <majklzumberi00@gmail.com>
2020-07-27 19:43:49 +0200

    added check form content test

--- cypress/integration/register.spec.js
+++ cypress/integration/register.spec.js
@@ -1,14 +1,32 @@
-/// <reference types="cypress"/>
 context('register', () => {
   beforeEach(() => {
     sessionStorage.removeItem('utente');
     cy.visit('http://localhost:4200/auth/register');
   });
+  it('check form content', () => {
+    cy.get('h3')
+    .should('have.text','Benvenuto')
+    .and('have.class','uk-card-title')
+    .and('have.class','uk-text-center')
+    .and('be.visible');
 
-  it('[sign-up] success', () => {
-    cy.get('.username-input').type('carlo').should('have.value','carlo');
-    cy.get('.password-input').type('123').should('have.value','123');
-    cy.get('.passwordCnf-input').type('123').should('have.value','123');
+    cy.get('form').within($form=>{
+      cy.wrap($form).should('be.visible');
+      cy.wrap($form).children().should('have.length', 4);
+      cy.wrap($form).find('button').last()
+      .should('be.disabled')
+      .and('have.text','Registrati')
+      .and('have.class','uk-button');
+
+      cy.wrap($form).find('.uk-text-small').last().should('have.text','già registrato? effettua il login')
+      cy.wrap($form).find('.uk-text-small').find('a').last().should('have.attr','routerlink','/auth/login')
+    });
+  });
+  it('sign-up success', () => {
+    cy.url().should('eq','http://localhost:4200/auth/register');
+    cy.get('.username-input').type('mario').should('have.value','mario').and('have.focus');
+    cy.get('.password-input').type('123').should('have.value','123').and('have.focus');
+    cy.get('.passwordCnf-input').type('123').should('have.value','123').and('have.focus');
     cy.server();
     const baseURL='http://localhost:3000/users';
     cy.route('POST',baseURL,{username:"carlo",password:"123",name:"",surname:""}).as('RegisterCall');
