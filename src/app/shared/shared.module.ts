682bebc81552a70f37afaed9572e8a8e45be64a8
majkl zumberi <majklzumberi00@gmail.com>
2020-08-03 23:53:34 +0200

    added calendar,datePicker,numericTextBox modules in shared.module.ts

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,6 +6,8 @@ import { TodoPreviewComponent } from './components/todo-preview/todo-preview.com
 import { TodoItemComponent } from './components/todo-item/todo-item.component';
 import { TodoFormComponent } from './components/todo-form/todo-form.component';
 import { MultiSelectAllModule } from '@syncfusion/ej2-angular-dropdowns';
+import {CalendarModule, DatePickerModule} from '@syncfusion/ej2-angular-calendars';
+import {NumericTextBoxModule} from '@syncfusion/ej2-angular-inputs';
 @NgModule({
   declarations: [
     InputTextComponent,
@@ -16,8 +18,11 @@ import { MultiSelectAllModule } from '@syncfusion/ej2-angular-dropdowns';
   imports: [
     CommonModule,
     FormsModule,
+    CalendarModule,
     ReactiveFormsModule,
-    MultiSelectAllModule
+    MultiSelectAllModule,
+    DatePickerModule,
+    NumericTextBoxModule
   ],
   exports: [
     CommonModule,
